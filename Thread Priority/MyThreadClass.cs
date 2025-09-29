41da7995e3e6567dd4247ae784ee352e6242a6a9
1jani5 <Johnarvinenriquez@gmail.com>
2025-09-29 19:01:13 +0800

    Add project files.

---
+++ Thread Priority/MyThreadClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Priority
+{
+
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int i = 1; i <= 2; i++)
+            {
+                Console.WriteLine("Thread 1: " + i);
+                System.Threading.Thread.Sleep(500);
+
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + i);
+
+            }
+        }
+
+        public static void Thread2()
+        {
+            for(int i = 1;i <= 6; i++)
+            {
+                Console.WriteLine("Thread 1: " + i);
+                System.Threading.Thread.Sleep(500);
+
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + i);
+            }
+        }
+    }
+}
